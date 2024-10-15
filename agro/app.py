e6708231d201bedeb6c3cf72e84f1496ff68996f
Shravani-Saratkar <164533558+Shravani-Saratkar@users.noreply.github.com>
2024-10-15 19:41:40 +0530

    .

--- agro/app.py
+++ agro/app.py
@@ -995,6 +995,6 @@ def commodity_profile():
         
 import os
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  
+    port = int(os.environ.get('PORT', 4000))  
     app.run(host='0.0.0.0', port=port)
 
